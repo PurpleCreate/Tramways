d343b70413e059e78f943d8c533de764d9227f6b
C1200 <ogjtudjy@gmail.com>
2024-11-23 12:19:23 +0000

    fix: f8938c9c didn't fix it

--- common/src/main/java/purplecreate/tramways/mixins/ScheduleRuntimeMixin.java
+++ common/src/main/java/purplecreate/tramways/mixins/ScheduleRuntimeMixin.java
@@ -5,6 +5,7 @@ import com.simibubi.create.content.trains.graph.DiscoveredPath;
 import com.simibubi.create.content.trains.schedule.ScheduleRuntime;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -13,10 +14,17 @@ import purplecreate.tramways.mixinInterfaces.IStopRequestableNavigation;
 
 @Mixin(value = ScheduleRuntime.class, remap = false)
 public class ScheduleRuntimeMixin {
+  @Unique private int tramways$lastEntry = -1;
   @Shadow private Train train;
+  @Shadow public int currentEntry;
 
   @Inject(method = "startCurrentInstruction", at = @At("HEAD"))
   private void tramways$startCurrentInstruction(CallbackInfoReturnable<DiscoveredPath> cir) {
+    if (tramways$lastEntry == currentEntry)
+      return;
+    
+    tramways$lastEntry = currentEntry;
+
     RequestStopServer.removeRequest(train);
     if (train.navigation instanceof IStopRequestableNavigation nav)
       nav.tramways$resetRouteCancelled();
