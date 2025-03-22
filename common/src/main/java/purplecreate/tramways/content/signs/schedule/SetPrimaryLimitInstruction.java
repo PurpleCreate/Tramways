8ff1ee6f48ef69738960e1c00a6032ce2a7102e2
C1200 <ogjtudjy@gmail.com>
2025-03-22 12:05:32 +0000

    fix: remove step on primary limit for more precise control

--- common/src/main/java/purplecreate/tramways/content/signs/schedule/SetPrimaryLimitInstruction.java
+++ common/src/main/java/purplecreate/tramways/content/signs/schedule/SetPrimaryLimitInstruction.java
@@ -1,6 +1,8 @@
 package purplecreate.tramways.content.signs.schedule;
 
 import com.simibubi.create.content.trains.schedule.destination.ChangeThrottleInstruction;
+import com.simibubi.create.foundation.gui.ModularGuiLineBuilder;
+import com.simibubi.create.foundation.utility.Lang;
 import net.minecraft.resources.ResourceLocation;
 import purplecreate.tramways.Tramways;
 
@@ -8,5 +10,15 @@ public class SetPrimaryLimitInstruction extends ChangeThrottleInstruction {
   @Override
   public ResourceLocation getId() {
     return Tramways.rl("set_primary_limit");
+  }
+
+  @Override
+  public void initConfigurationWidgets(ModularGuiLineBuilder builder) {
+    builder.addScrollInput(0, 50, (si, l) -> {
+      si
+        .withRange(5, 101)
+        .titled(Lang.translateDirect("schedule.instruction.throttle_edit_box"));
+      l.withSuffix("%");
+    }, "Value");
   }
 }
