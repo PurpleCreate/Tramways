5a3213ca2e2aa2cf03c94b1c1cb8bea102f49a22
C1200 <40738690+C1200@users.noreply.github.com>
2026-06-22 13:16:32 +0100

    update route config screen

--- common/src/main/java/purplecreate/tramways/content/signals/routing/RouteConfiguratorScreen.java
+++ common/src/main/java/purplecreate/tramways/content/signals/routing/RouteConfiguratorScreen.java
@@ -4,18 +4,33 @@ import com.simibubi.create.content.trains.signal.SignalBlockEntity;
 import com.simibubi.create.foundation.gui.AllIcons;
 import com.simibubi.create.foundation.gui.widget.IconButton;
 import com.simibubi.create.foundation.gui.widget.ScrollInput;
+import com.simibubi.create.foundation.gui.widget.TooltipArea;
 import net.createmod.catnip.gui.AbstractSimiScreen;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
 import purplecreate.tramways.TGuiSprites;
 import purplecreate.tramways.TNetworking;
+import purplecreate.tramways.Tramways;
 import purplecreate.tramways.content.signals.base.JunctionState;
 import purplecreate.tramways.content.signals.base.JunctionState.DirectionalJunctionState;
 import purplecreate.tramways.mixinInterfaces.IRoutedSignalBlock;
 
+import java.util.List;
+
 public class RouteConfiguratorScreen extends AbstractSimiScreen {
   private static final TGuiSprites bg = TGuiSprites.ROUTE_CONFIGURATOR;
+
+  private static final MutableComponent title = Tramways.translatable("route_configurator.modal_title");
+  private static final MutableComponent nameTitle = Tramways.translatable("route_configurator.name_title");
+  private static final MutableComponent nameHint = Tramways.translatable("route_configurator.name_hint");
+  private static final MutableComponent letterTitle = Tramways.translatable("route_configurator.letter_title");
+  private static final MutableComponent letterHint = Tramways.translatable("route_configurator.letter_hint");
+  private static final MutableComponent directionTitle = Tramways.translatable("route_configurator.direction_title");
+  private static final MutableComponent directionHint = Tramways.translatable("route_configurator.direction_hint");
 
   private final SignalBlockEntity signal;
   private EditBox nameBox;
@@ -39,21 +54,38 @@ public class RouteConfiguratorScreen extends AbstractSimiScreen {
         .withCallback(this::onClose)
     );
 
-    nameBox = makeEditBox(56, 95, 64);
+    nameBox = makeEditBox(58, 91, 64);
     nameBox.setValue(settings == null ? "" : settings.getName().getString());
+    addRenderableWidget(
+      new TooltipArea(guiLeft + 56, guiTop + 25, 95, 16)
+        .withTooltip(List.of(
+          nameTitle.plainCopy().withStyle((s) -> s.withColor(TooltipArea.HEADER_RGB.getRGB())),
+          nameHint.plainCopy().withStyle((s) -> s.withColor(TooltipArea.HINT_RGB.getRGB()))
+        ))
+    );
 
-    letterBox = makeEditBox(159, 16, 1);
+    letterBox = makeEditBox(161, 12, 1);
     letterBox.setValue(settings == null ? "" : String.valueOf(settings.getLetter()));
+    addRenderableWidget(
+      new TooltipArea(guiLeft + 159, guiTop + 25, 16, 16)
+        .withTooltip(List.of(
+          letterTitle.plainCopy().withStyle((s) -> s.withColor(TooltipArea.HEADER_RGB.getRGB())),
+          letterHint.plainCopy().withStyle((s) -> s.withColor(TooltipArea.HINT_RGB.getRGB()))
+        ))
+    );
 
-    directionBox = new ScrollInput(guiLeft + 184, guiTop + 24, 16, 16)
+    directionBox = new ScrollInput(guiLeft + 184, guiTop + 25, 16, 16)
+      .titled(directionTitle)
+      .addHint(directionHint)
       .inverted()
+      .withShiftStep(1)
       .withRange(0, DirectionalJunctionState.values().length)
       .setState((settings == null ? DirectionalJunctionState.NONE : settings.getDirection()).ordinal());
     addRenderableWidget(directionBox);
   }
 
   private EditBox makeEditBox(int x, int width, int maxLength) {
-    EditBox box = new EditBox(font, guiLeft + x, guiTop + 28, width, 10, Component.empty());
+    EditBox box = new EditBox(font, guiLeft + x, guiTop + 29, width, 10, Component.empty());
     box.setTextColor(-1);
     box.setTextColorUneditable(-1);
     box.setBordered(false);
@@ -66,8 +98,11 @@ public class RouteConfiguratorScreen extends AbstractSimiScreen {
 
   @Override
   protected void renderWindow(GuiGraphics graphics, int mouseX, int mouseY, float partialTicks) {
-    bg.blit(graphics, 0, 0);
-    graphics.blit(bg.getPath(), guiLeft + 184, guiTop + 24, directionBox.getState() * 17, 85, 16, 16);
+    Font font = Minecraft.getInstance().font;
+
+    bg.blit(graphics, guiLeft, guiTop);
+    graphics.blit(bg.getPath(), guiLeft + 184, guiTop + 25, directionBox.getState() * 17, 85, 16, 16);
+    graphics.drawString(font, title, Math.round(guiLeft + bg.getWidth() / 2f - font.width(title) / 2f), guiTop + 4, 0x442000, false);
   }
 
   @Override
